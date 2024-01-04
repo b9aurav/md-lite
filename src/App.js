44ce51ffdfab544fe8139ceb3b36bf543a05684a
Gaurav <hey9aurav@gmail.com>
2024-01-04 11:43:51 +0530

    Frontend bug fixes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
-import { MdInfoOutline } from "react-icons/md";
+import { MdInfoOutline, MdOutlineSave } from "react-icons/md";
 
 function App() {
   return (
     <div className="main center-items">
-      <div className="header">
-        <button><MdInfoOutline /></button>
+      <div className="header center-items">
+        <button className="float-left"><MdInfoOutline /></button>
         <label>
-          Demo editor by <a href="https://github.com/b9aurav">Gaurav</a>
+          Demo editor by <br/><a href="https://github.com/b9aurav">Gaurav</a>
         </label>
-        <button className="float-right">Save</button>
+        <button className="float-right"><MdOutlineSave/> Save</button>
       </div>
       <div className="textarea center-items">
         <textarea></textarea>
