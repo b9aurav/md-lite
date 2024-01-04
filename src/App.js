d0ccc92f96f54bc3360d8df0a4c9675bb92b1ebd
Gaurav <hey9aurav@gmail.com>
2024-01-04 11:31:25 +0530

    Base Frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { MdInfoOutline } from "react-icons/md";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main center-items">
+      <div className="header">
+        <button><MdInfoOutline /></button>
+        <label>
+          Demo editor by <a href="https://github.com/b9aurav">Gaurav</a>
+        </label>
+        <button className="float-right">Save</button>
+      </div>
+      <div className="textarea center-items">
+        <textarea></textarea>
+      </div>
     </div>
   );
 }
